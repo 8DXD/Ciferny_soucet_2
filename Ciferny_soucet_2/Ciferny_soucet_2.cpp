91ea089935e9922345706e0cac5558adcd1422ab
Kateřina Dvořáková <kacule05@gmail.com>
2022-06-20 18:31:04 +0200

    Přidejte soubory projektu.

---
+++ Ciferny_soucet_2/Ciferny_soucet_2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int soucet = 0;
+    int cislo;
+    printf("Zadejte cislo, jehoz ciferny soucet chcete vypocitat: ");
+    scanf_s("%d", &cislo);
+    while (cislo > 0) {
+        soucet += (cislo % 10);
+        cislo /= 10;
+    }
+    printf("\nCiferny soucet: %d", soucet);
+}
